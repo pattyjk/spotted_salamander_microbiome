d4e32767c83f55f31943b1edca661ddcd7156a08
Patrick Kearns <kearnspj23@gmail.com>
2023-09-28 21:05:20 -0400

    up

--- spotted_sal_r_code.R
+++ spotted_sal_r_code.R
@@ -18,6 +18,7 @@ dim(asv.tbl)
 #2106  225
 
 #rarefy table
+set.seed(555)
 asv.rare<-rrarefy(t(asv.tbl), sample = 400)
 dim(asv.tbl)
 
@@ -29,7 +30,69 @@ larv_tbl<-asv.rare[row.names(asv.rare) %in% larv_meta$SampleID, ]
 
 #create asv table for juvenile experiment
 juv_tbl<-asv.rare[row.names(asv.rare) %in% juv_meta$SampleID, ]
-View(juv_tbl)
+
+#calculate beta diversity
+juv.pcoa<-capscale(juv_tbl~1, method='jaccard')
+life.pcoa<-capscale(life_stg_tbl~1, method='jaccard')
+larv.pcoa<-capscale(larv_tbl~1, method='jaccard')
+
+#calculate percent variation explained
+100*round(juv.pcoa$CA$eig[1]/sum(juv.pcoa$CA$eig), 3)
+#22.6
+100*round(juv.pcoa$CA$eig[2]/sum(juv.pcoa$CA$eig), 3)
+#12.8
+
+100*round(life.pcoa$CA$eig[1]/sum(life.pcoa$CA$eig), 3)
+#23.9
+100*round(life.pcoa$CA$eig[2]/sum(life.pcoa$CA$eig), 3)
+#13.6
+
+100*round(larv.pcoa$CA$eig[1]/sum(larv.pcoa$CA$eig), 3)
+#42.4
+100*round(larv.pcoa$CA$eig[2]/sum(larv.pcoa$CA$eig), 3)
+#22.9
+
+
+#extract the coordinates
+larv.scores<-scores(larv.pcoa)
+larv.coords<-as.data.frame(larv.scores$sites)
+larv.coords$SampleID<-rownames(larv.coords)
+larv.coords<-merge(larv.coords, larv_meta, by=c('SampleID'))
+
+life.scores<-scores(life.pcoa)
+life.coords<-as.data.frame(life.scores$sites)
+life.coords$SampleID<-rownames(life.coords)
+life.coords<-merge(life.coords, life_meta, by=c('SampleID'))
+
+juv.scores<-scores(juv.pcoa)
+juv.coords<-as.data.frame(juv.scores$sites)
+juv.coords$SampleID<-rownames(juv.coords)
+juv.coords<-merge(juv.coords, juv_meta, by=c('SampleID'))
+
+
+ggplot(juv.coords, aes(MDS1, MDS2, color=Treatment))+  
+  geom_point(aes(size=2))+
+  theme_bw()+
+  xlab("PC1-%")+
+  ylab("PC2-%")+
+  theme(text = element_text(size=14),
+        axis.text = element_text(size=14), legend.text=element_text(size=14))
+
+ggplot(larv.coords, aes(MDS1, MDS2, color=Treatment2))+  
+  geom_point(aes(size=2))+
+  theme_bw()+
+  xlab("PC1-%")+
+  ylab("PC2-%")+
+  theme(text = element_text(size=14),
+        axis.text = element_text(size=14), legend.text=element_text(size=14))
+
+ggplot(life.coords, aes(MDS1, MDS2, color=LifeStage))+  
+  geom_point(aes(size=2))+
+  theme_bw()+
+  xlab("PC1-23.9%")+
+  ylab("PC2-13.6%")+
+  theme(text = element_text(size=14),
+        axis.text = element_text(size=14), legend.text=element_text(size=14))
 
 #######################analyze taxonomy
 
